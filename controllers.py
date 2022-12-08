6bed66748e3e7d9177aaa731f13b06b99f524549
jjcarrano <116608116+jjcarrano@users.noreply.github.com>
2022-12-07 22:50:02 -0600

    Update controllers.py

--- controllers.py
+++ controllers.py
@@ -77,6 +77,8 @@ class _TabController:
         self._tab.adjustmentsPanel.saturationSliderGroup.bind_callback(self.saturation_slider_callback)
         self._tab.adjustmentsPanel.warmthSliderGroup.bind_callback(self.warmth_slider_callback)
         self._tab.adjustmentsPanel.tintSliderGroup.bind_callback(self.tint_slider_callback)
+        self._tab.adjustmentsPanel.twoToneHueSliderGroup.bind_callback(self.two_tone_hue_callback)
+        self._tab.adjustmentsPanel.twoToneSaturationSliderGroup.bind_callback(self.two_tone_saturation_callback)
         self._tab.adjustmentsPanel.bind_checkbox(self.checkbox_callback)
         self._model.add_processedImage_callback(self.processed_image_callback)
         self._model.add_existUnsavedChanges_callback(self.unsaved_changes_callback)
@@ -117,6 +119,16 @@ class _TabController:
         x = (event-.5)*2
         tintFactor = (x**3+x)/2
         self._model.change_processing_params({models.ParamType.TINT: tintFactor})
+
+    def two_tone_hue_callback(self, event):
+        event = float(event)
+        twoToneHue = (event-.5)*180
+        self._model.change_processing_params({models.ParamType.TWO_TONE_HUE: twoToneHue})
+
+    def two_tone_saturation_callback(self, event):
+        event = float(event)
+        twoToneSaturation = 1.5*exp((event-.5)*log(9))-.5
+        self._model.change_processing_params({models.ParamType.TWO_TONE_SATURATION: twoToneSaturation})
 
     def checkbox_callback(self):
         if self._tab.adjustmentsPanel.checkboxChecked:
