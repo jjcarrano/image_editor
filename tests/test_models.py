8a29905053911f20ca343fb928897e9cc49e7c0f
Jacob Carrano <jjcarrano@gmail.com>
2024-11-09 17:22:49 -0500

    added test for UniquePixelData

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,13 +2,19 @@ from src.models import _UniquePixelData
 import numpy as np
 
 
-def test_UniquePixelData():
-    pixels = np.random.randint(256, size=(1000, 3), dtype=np.uint8)
-    uniquePixelData = _UniquePixelData(pixels)
-    uniquePixelsNumpy = np.unique(pixels, axis=0)
-    # conditions required for uniquePixelData.values to be equivalent to np.unique output ignoring row order
-    passConditions = [
-        np.array_equal(np.unique(uniquePixelData.values, axis=0), uniquePixelsNumpy),
-        uniquePixelData.values.shape == uniquePixelsNumpy.shape
-    ]
-    assert all(passConditions)
+class TestUniquePixelData:
+    def test__init__(self):
+        pixels = np.random.randint(256, size=(1000, 3), dtype=np.uint8)
+        uniquePixelData = _UniquePixelData(pixels)
+        uniquePixelsNumpy = np.unique(pixels, axis=0)
+        # conditions required for uniquePixelData.values to be equivalent to np.unique output ignoring row order
+        passConditions = [
+            np.array_equal(np.unique(uniquePixelData.values, axis=0), uniquePixelsNumpy),
+            uniquePixelData.values.shape == uniquePixelsNumpy.shape
+        ]
+        assert all(passConditions)
+
+    def test_reverse(self):
+        pixels = np.random.randint(256, size=(1000, 3), dtype=np.uint8)
+        uniquePixelData = _UniquePixelData(pixels)
+        assert np.array_equal(pixels, uniquePixelData.reverse())
